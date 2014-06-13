635ce964074d7da2bac38882b3d87eaddf3486b3
Hack Reactor Students <kiafathi@me.com>
2014-06-12 19:21:17 -0700

    implement n-queens solution

--- src/solvers.js
+++ src/solvers.js
@@ -37,52 +37,68 @@ window.countNRooksSolutions = function(n) {
   var solutionCount = 0;
 
 
-  var generator = function (numSpaces, playsMade, matrix){
-    // break recursion under 2 criteria
-    // if (n===2) {debugger;};
+  var generator = function (position, playsMade, matrix){
 
-    if(numSpaces + 1 < (n - playsMade)){
+    //Should check that the remaining spaces are < plays remaining
+    if((n*n) - position < (n - playsMade)){
       return;
     }
 
 
-    if (numSpaces < 0 || playsMade === n) {
-      if ( playsMade === n ) {
-        var board = new Board(matrix);
-        if (!board.hasAnyRooksConflicts()) {
-          debugger;
-          solutionCount++;
-        }
+    // //Make sure not to pass pos that is out of board
+    if(position + 1 > n*n){
+      return;
+    }
+
+    //call generator with unchanged board
+    position++;
+
+    generator(position, playsMade, matrix);
+    position--;
+
+    //change board and call generator
+    var newMatrix = JSON.parse(JSON.stringify(matrix));
+    newMatrix[(position - (position % n))/n][position % n] = 1;
+    var newPlays = playsMade + 1;
+
+    //Checks to see if we are out of plays. If so we may have a solution
+    if (newPlays === n) {
+      var board = new Board(newMatrix);
+      if (!board.hasAnyRooksConflicts()) {
+        solutionCount++;
       }
       return;
     }
 
+    //otherwise we move and pass the new board down
+    var moveFactor;
+    if (position % n === 0){
+      moveFactor = n + 1;
+    } else {
+      moveFactor = (n - (position % n));
+    }
 
-    if (numSpaces < 0) {
+    var newPosition = position + moveFactor;
+
+    //Make sure not to pass pos that is out of board
+    if(newPosition > n*n){
       return;
     }
 
-    numSpaces--;
-    generator(numSpaces, playsMade, matrix);
-    numSpaces++;
+    //call generator with new state
+    generator(newPosition, newPlays, newMatrix);
 
-    matrix[(numSpaces - (numSpaces % n))/n][numSpaces % n] = 1;
 
-    numSpaces--;
-    generator(numSpaces, playsMade+1, matrix);
-    numSpaces++;
-
-    matrix[(numSpaces - (numSpaces % n))/n][numSpaces % n] = 0;
 
   };
 
 
   var empty = makeEmptyMatrix(n);
 
-  // if (n === 1) {
-  //   return 1;
-  // }
-  generator((n*n)-1, 0, empty);
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  generator(0, 0, empty);
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
@@ -92,8 +108,7 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
-
+  var solution;
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
@@ -101,8 +116,66 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  if (n === 0) {
+    return 1;
+  }
+  var solutionCount = 0;
+
+
+  var nQ = function(row, colC, maDC, miDC) {
+    // if (n===2) { debugger; }
+    var row = row || 0;
+    var colC = colC || {};
+    var maDC = maDC || {};
+    var miDC = miDC || {};
+
+
+
+    for (var i = 0; i < n; i ++) {
+      if (!colC[i] && !maDC[i - row] && !miDC[row + i]) {
+        if (row + 1 === n) {
+          // debugger;
+          solutionCount ++;
+          return;
+        }
+        colC[i] = true;
+        maDC[i - row] = true;
+        miDC[row  + i] = true;
+        nQ(row + 1, colC, maDC, miDC);
+        colC[i] = false;
+        maDC[i - row] = false;
+        miDC[row + i] = false;
+      }
+    }
+  };
+
+  nQ();
+
+
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
